35bb1a110a86742dc9333a040a8925043371bd7b
Simone Seria <simoneseria@live.com>
2024-07-11 00:04:45 +0200

    commmit 11 luglio

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,7 @@
 import React, { userState} from 'react'
 import Logo from '../../assets/instrumental.svg'
 import styles from './NavBar.module.css'
+import SearchBar from '../SearchBar/SearchBar';
 
 function NavBar() {
     return(
@@ -11,17 +12,19 @@ function NavBar() {
 
         <nav className={styles.menu}>
             <ul>
-                <li><a href="">Home</a></li>
-                <li><a href="">Services</a></li>
-                <li><a href="">About</a></li>
+                <li><a href="home">Home</a></li>
+                <li><a href="services">Services</a></li>
+                <li><a href="about">About</a></li>
             </ul>
         </nav>
         
+        <SearchBar/>
+
         <div className={styles.buttons}>
             <a href=""> Register </a>
             <a href=""> Login </a>
         </div>
-
+        
         </header>
         </>
     );
---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,14 @@
+import React, { userState} from 'react'
+import styles from './SearchBar.module.css'
+
+function SearchBar(){
+
+    return(
+        <>
+            <input type="text" placeholder="Search.." className={styles.searchbar}/>
+        </>
+    );
+
+};
+
+export default SearchBar;
